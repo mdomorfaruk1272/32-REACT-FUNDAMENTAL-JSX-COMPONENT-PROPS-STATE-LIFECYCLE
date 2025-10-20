5e45e04e910a475ae8072f483862a589d95dfadb
mdomorfaruk1272 <mdomorfarukg1272@gmail.com>
2025-10-20 22:38:22 +0600

    32-4-JSX-Dynamic-content-Dynamic-Style-in-React

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,33 @@
-import React from 'react'
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
+/**
+ * second bracket({}) is used to use variable on jsx html / used to make content dynamic or display dynamic data. Like: <p>{name}</p>
+ * For inline style in react jsx we use an object of style with (key, value). Like: const style={color:"red"}
+ * In component style attribute is dynamic for that we need to use second bracket after equal sign. Like: <p style={style}></p> 
+ * @returns 
+ */
+
 function App() {
-  const [count, setCount] = useState(0)
+  var name = "Md. Omor Faruk";
+  var person = {
+    name: 'Dr. Mahfuz',
+    job: 'Singer',
+  }
+
+  var person2 = {
+    name: "Eva Rahman",
+    job: "kokil kondi",
+  }
+
+  var style = {
+    color: 'red',
+    backgroundColor: 'yellow',
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div style={{backgroundColor:'green'}}>
+    <h1>i am a react person {name}</h1>
+    </div>
   )
 }
 

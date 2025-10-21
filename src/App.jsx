1b3840d4981aae9ddb582db5e42d693dd338b9b2
mdomorfaruk1272 <mdomorfarukg1272@gmail.com>
2025-10-21 10:12:34 +0600

    32-8 Pass object to components and access object

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
 import './App.css'
 
 /**
- * pass dyanmic data in component by kay and value;
+ * we can pass object array in props
  */
 
 function App() {
   const nayoks = ["Anwar", "Jafor", "Alomgir", "Salman"];
+  const products = [
+    {name: 'photoshop', price: '$90.99'},
+    {name: 'illustrator', price: '$60.99'},
+    {name: 'PDF Reader', price: '$6.99'}
+  ]
+
   return (
     <div>
     <p>I am a React Person</p>
+    <Product product={products[0]}></Product>
+    <Product product={products[1]}></Product>
     <Person name="Munna" job="football"></Person>
     <Person name="Masum" job="Dorshok"></Person>
     <Person></Person>
@@ -16,12 +24,26 @@ function App() {
   )
 }
 
-/**
- * props used to get dynamic data in Component. It is a object. key is the component attribute 
- * and value is the component attribute value.
- * @param {*} props 
- * @returns 
- */
+function Product(props){
+  const productStyle = {
+    border: '1px solid gray',
+    borderRadius: '5px',
+    backgroundColor: 'lightgray',
+    height: '200px',
+    width: '200px',
+    float: 'left',
+    display: 'block',
+  }
+  const {name, price} = props.product;
+  console.log(name, price);
+  return (
+    <div style={productStyle}>
+    <h3>{props.product.name}</h3>
+    <h1>{props.product.price}</h1>
+    <button>Buy now</button>
+    </div>
+  )
+}
 
 function Person(props){
   return(

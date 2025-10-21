a0b72ff33217af8fc78f5a7934de6767a59afe8e
mdomorfaruk1272 <mdomorfarukg1272@gmail.com>
2025-10-21 10:41:53 +0600

    C:\Users\Md. Al- Amin\Desktop\Jhanker Mahbub\jm-5\32 REACT FUNDAMENTAL, JSX, COMPONENT, PROPS, STATE, LIFECYCLE\core-concepts>git branch -m 32-9-Create-multiple-components-from-an-array-of-objects

--- src/App.jsx
+++ src/App.jsx
@@ -5,21 +5,61 @@ import './App.css'
  */
 
 function App() {
-  const nayoks = ["Anwar", "Jafor", "Alomgir", "Salman"];
+  const nayoks = ["Anwar", "Jafor", "Alomgir", "Salman", "Bappi"];
   const products = [
     {name: 'photoshop', price: '$90.99'},
     {name: 'illustrator', price: '$60.99'},
-    {name: 'PDF Reader', price: '$6.99'}
+    {name: 'PDF Reader', price: '$6.99'},
+    {name: 'Premiere El', price: '$249.99'}
   ]
-
+  const friends = [
+    {name: 'Md. Hasan Ali', age: 17},
+    {name: 'Md. Kalam Ali', age: 18},
+    {name: 'Md. Salam Ali', age: 19},
+    {name: 'Md. Jaman Ali', age: 20},
+    {name: 'Md. Hamim Ali', age: 21},
+    {name: 'Md. Rakib Ali', age: 22},
+  ]
+  const friendsHtml = [];
+  for (let index = 0; index < friends.length; index++) {
+    const element = friends[index];
+    friendsHtml.push(<Friend friend={element}></Friend>)
+  }
+  console.log(friendsHtml);
+  // const productsName = products.map(product => product.name);
+  // console.log(productsName);
+  const nayoksName = nayoks.map(nayok => nayok);
+  console.log(nayoksName);
   return (
     <div>
     <p>I am a React Person</p>
-    <Product product={products[0]}></Product>
-    <Product product={products[1]}></Product>
-    <Person name="Munna" job="football"></Person>
-    <Person name="Masum" job="Dorshok"></Person>
+    <ul>
+      {
+        nayoks.map(nayoks => <li>{nayoks}</li>)
+      }
+      {
+        products.map(product => <li>{product.name}</li>)
+      }
+    </ul>
+    {
+      products.map(product => <Product product={product}></Product>)
+    }
     <Person></Person>
+    {
+      friends.map(friend => <Friend friend={friend}></Friend>)
+    }
+    {/* {
+     friendsHtml
+    } */}
+    </div>
+  )
+}
+
+function Friend(props){
+  return (
+    <div style={{backgroundColor:'lightsalmon', borderRadius:'10px', margin: '10px', padding: '15px', boxShadow: '5px 5px 10px gray'}}>
+      <h1>Name: {props.friend.name}</h1>
+      <h3>Age: {props.friend.age}</h3>
     </div>
   )
 }
@@ -29,13 +69,12 @@ function Product(props){
     border: '1px solid gray',
     borderRadius: '5px',
     backgroundColor: 'lightgray',
-    height: '200px',
-    width: '200px',
+    height: '300px',
+    width: '300px',
     float: 'left',
     display: 'block',
   }
   const {name, price} = props.product;
-  console.log(name, price);
   return (
     <div style={productStyle}>
     <h3>{props.product.name}</h3>
